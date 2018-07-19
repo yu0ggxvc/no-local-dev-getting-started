45d965bcdeb888f5283e3d559279a4b60a1199b3
yu0ggxvc <41416961+yu0ggxvc@users.noreply.github.com>
2018-07-19 15:45:34 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
 end
 
-#get "/contacts" do
-#  @contacts = Contact.all
-#  erb :index
-#end
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+end
 
 get "/create" do
   dashboard_url = 'https://dashboard.heroku.com/'
@@ -25,3 +25,4 @@ get "/create" do
   dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
   redirect to(dashboard_url)
 end
+
